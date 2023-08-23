3298bc8014f5f16a1afe43acc0c4e96275c1286c
Vancea Andreea <andreea_vancea28@yahoo.com>
2023-08-23 13:37:51 +0300

    Ex1

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        Student s1= new Student("Andreea",9);
+        Student s2= new Student("Alex",8);
+        Student s3= new Student("Ion",10);
+
+        Student[] studentArray=new Student[3];
+        studentArray[0]=s1;
+        studentArray[1]=s2;
+        studentArray[2]=s3;
+
+        Student.saveStudents(studentArray);
+
+       studentArray=Student.loadStudents();
+
+        if(studentArray != null){
+            System.out.println("Loaded students: ");
+            for(Student student:studentArray){
+                System.out.println(student);
+            }
+        }
+
+    }
+}
---
+++ src/Student.java
@@ -0,0 +1,62 @@
+import java.io.*;
+
+public class Student implements Serializable {
+    private String name;
+    private double grade;
+    private static Student[] studentsArray;
+
+    public Student(String name, double grade) {
+        this.name = name;
+        this.grade = grade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getGrade() {
+        return grade;
+    }
+
+    public void setGrade(double grade) {
+        this.grade = grade;
+    }
+
+    public static void saveStudents(Student[] studentsArray){
+        try{
+        FileOutputStream fileOutputStream=new FileOutputStream("student.data");
+        ObjectOutputStream objectOutputStream=new ObjectOutputStream(fileOutputStream);
+        objectOutputStream.writeObject(studentsArray);
+
+        System.out.println("The students have been saved.");
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    public static Student[] loadStudents(){
+        Student[] loadedStudents=null;
+        try{
+            FileInputStream fileInputStream=new FileInputStream("student.data");
+            ObjectInputStream objectInputStream=new ObjectInputStream(fileInputStream);
+            loadedStudents= (Student[]) objectInputStream.readObject();
+
+            System.out.println("The students have been loaded.");
+        }
+        catch(IOException | ClassNotFoundException e){
+            e.printStackTrace();
+        }
+        return loadedStudents;
+    }
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", grade=" + grade +
+                '}';
+    }
+}
